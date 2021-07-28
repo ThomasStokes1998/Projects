344d45d419a431755c8d7605f9febba6dbc37793
Thomas Stokes <70324434+ThomasStokes1998@users.noreply.github.com>
2021-07-28 08:15:56 +0100

    Add files via upload

--- Speed Solving Analysis/wca_elo.py
+++ Speed Solving Analysis/wca_elo.py
@@ -5,7 +5,10 @@ from datetime import datetime
 
 # Data Frames
 results = pd.read_csv('WCA Database/WCA_export_Results.tsv', delimiter='\t')
+competitions = pd.read_csv('WCA Database/WCA_export_Competitions.tsv', delimiter='\t')
+championships = pd.read_csv('WCA Database/WCA_export_championships.tsv', delimiter='\t')
 
+champs = championships.competition_id.unique()
 # Dictionaries
 events = results.eventId.unique()
 len_events = len(events)
@@ -17,25 +20,54 @@ print("Events dictionary finished.")
 
 print("ELO dictionary started.")
 elo_dict = {}
-scratch = True
+scratch = False
 persons = results.personId.nunique()
 personlist = results.personId.unique()
 if not scratch:
-    elotable400 = pd.read_csv("elotable_400.csv")
-    for wcaid in list(elotable400.columns):
-        elo_dict[wcaid] = list(elotable400[wcaid])
+    elotable_scratch = pd.read_csv("elotable_7000.csv")
+    for wcaid in list(elotable_scratch.columns):
+        elo_dict[wcaid] = list(elotable_scratch[wcaid])
 print("ELO dictionary finished.")
 
 
 # Functions
+def compdate(comp):
+    df = competitions[competitions.id == comp].reset_index(drop="index")
+    month = df.month[0]
+    year = df.year[0]
+    date = str(month) + "_" + str(year)
+    return date
+
+
 def elo(home, away, lam=0.02):
     h = 1 - 1 / (1 + np.exp(lam * (away - home)))
     a = - 1 / (1 + np.exp(lam * (home - away)))
     return [h, a]
 
+
+def elo_weight(rtype, competition):
+    mult = 1
+    if competition in champs:
+        mult = 1.5
+    if "wc20" in competition.lower():
+        mult = 2
+    if rtype == "1":
+        return 10 * mult
+    if rtype == "2":
+        return 20 * mult
+    if rtype == "3":
+        return 25 * mult
+    if rtype == "f":
+        return 50 * mult
+    else:
+        return 20 * mult
+
+
 def round_finder(competition, event, rtype=""):
     df = results[results["competitionId"] == competition].reset_index(drop="index")
-    df = df[["eventId", "roundTypeId", "pos", "personId"]]
+    df = df[["eventId", "roundTypeId", "pos", "personId", "regionalSingleRecord", "regionalAverageRecord"]]
+    df["regionalSingleRecord"] = df["regionalSingleRecord"].fillna("0")
+    df["regionalAverageRecord"] = df["regionalAverageRecord"].fillna("0")
     dfe = df[df["eventId"] == event].reset_index(drop="index")
     if rtype == "":
         return dfe
@@ -44,17 +76,19 @@ def round_finder(competition, event, rtype=""):
         return dfer
 
 
-def elo_calculator(competition, lam=0.2, mult=1600, eps=0.9999):
+def elo_calculator(competition, lam=0.02):
     df = results[results["competitionId"] == competition].reset_index(drop="index")
     df_events = df.eventId.unique()
     for event in df_events:
         edex = events_dict[event]
         dfe = round_finder(competition, event)
-        eventids = dfe.personId.unique()
         for rtype in dfe.roundTypeId.unique():
             dfer = round_finder(competition, event, rtype)
+            dfer = dfer.sort_values("pos").reset_index(drop='index')
+            elow = elo_weight(rtype, competition)
             for i in range(len(dfer.pos)):
                 homewcaid = dfer.personId[i]
+                # Adds new WCAID
                 if homewcaid not in elo_dict.keys():
                     elo_dict[homewcaid] = [-1] * len(events)
                 if elo_dict[homewcaid][edex] == -1:
@@ -62,21 +96,32 @@ def elo_calculator(competition, lam=0.2, mult=1600, eps=0.9999):
                 for j in range(len(dfer.pos)):
                     if i < j:
                         awaywcaid = dfer.personId[j]
+                        # Adds new WCAID
                         if awaywcaid not in elo_dict.keys():
                             elo_dict[awaywcaid] = [-1] * len(events)
                         if elo_dict[awaywcaid][edex] == -1:
                             elo_dict[awaywcaid][edex] = 1000
                         points = elo(elo_dict[homewcaid][edex], elo_dict[awaywcaid][edex], lam)
-                        elo_dict[homewcaid][edex] += mult / len(dfer) * points[0]
-                        if elo_dict[awaywcaid][edex] + mult / len(dfer) * points[1] < 0:
+                        # Points for 'home'
+                        elo_dict[homewcaid][edex] += elow * points[0]
+                        # Points for 'away'
+                        if elo_dict[awaywcaid][edex] + elow * points[1] < 0:
                             elo_dict[awaywcaid][edex] = 0
                         else:
-                            elo_dict[awaywcaid][edex] += mult / len(dfer) * points[1]
-        for wcaid in list(elo_dict.keys()):
-            if wcaid not in eventids:
-                score = elo_dict[wcaid][edex]
-                if score > 0:
-                    elo_dict[wcaid][edex] = score * eps
+                            elo_dict[awaywcaid][edex] += elow * points[1]
+                # Points for records
+                if dfer["regionalSingleRecord"][i] == "NR":
+                    elo_dict[homewcaid][edex] += 10
+                if dfer["regionalSingleRecord"][i] == "WR":
+                    elo_dict[homewcaid][edex] += 50
+                elif dfer["regionalSingleRecord"][i] != "0":
+                    elo_dict[homewcaid][edex] += 20
+                if dfer["regionalAverageRecord"][i] == "NR":
+                    elo_dict[homewcaid][edex] += 10
+                if dfer["regionalAverageRecord"][i] == "WR":
+                    elo_dict[homewcaid][edex] += 50
+                elif dfer["regionalAverageRecord"][i] != "0":
+                    elo_dict[homewcaid][edex] += 20
 
 
 # Calculates the ELO score for every competitor
@@ -84,18 +129,29 @@ count = 0
 if scratch:
     start_val = 0
 else:
-    start_val = 0
+    start_val = 7000
 total_comps = results.competitionId.nunique()
 list_comps = results.competitionId.unique()
 init_t = datetime.now()
+comp_date = "07_2021"
 start = True
 if start:
     print("Started")
     for competition in list_comps:
         if count > start_val:
             elo_calculator(competition)
+            # Depreciates existing scores
+            comp_date1 = compdate(competition)
+            if comp_date1 != comp_date:
+                for wcaid in list(elo_dict.keys()):
+                    for e in range(len(events)):
+                        score = elo_dict[wcaid][e]
+                        if score > 0:
+                            elo_dict[wcaid][e] = score * 0.995
+            comp_date = comp_date1
         count += 1
         if count % int(total_comps / 1000) == 0 and count > start_val:
+            print("-" * 30 + comp_date + "-" * 30)
             print("Last competition entered: " + competition)
             print("Percent complete: " + str(round(100 * count / total_comps, 2)) + "%")
             if count == start_val + 7 - start_val % 7:
@@ -107,33 +163,22 @@ if start:
         if count % 100 == 0 and count > start_val:
             elotable = pd.DataFrame(elo_dict)
             elotable.to_csv("elotable_" + str(count) + ".csv", index=False)
-            print("=" * 50)
-            print("Elapsed time for " + str(count) + " comps:", datetime.now() - init_t)
-            print("=" * 50)
+            if count - start_val == 100:
+                print("=" * 20 + competition + "=" * 20)
+                print("Elapsed time for " + str(count) + " comps:", datetime.now() - init_t)
+                print("=" * 20 + competition + "=" * 20)
+                m = datetime.now()
+            else:
+                print("=" * 20 + competition + "=" * 20)
+                print("Elapsed time for " + str(count) + " comps:", datetime.now() - m)
+                print("=" * 20 + competition + "=" * 20)
+                m = datetime.now()
+    # Complete ELO Table
+    elotable.to_csv("elotable_" + str(count) + ".csv", index=False)
     print("Finished")
     print("Elapsed time:", datetime.now() - init_t)
 
 # Creates the CSV file
-if not start:
-    print("Started WC2003")
-    elo_calculator("WC2003", 0.2, 1600)
-    print("Started WC2005")
-    elo_calculator("WC2005", 0.2, 1600)
-    print("Started WC2007")
-    elo_calculator("WC2007", 0.2, 1600)
-    print("Started WC2009")
-    elo_calculator("WC2009", 0.2, 1600)
-    print("Started WC2011")
-    elo_calculator("WC2011", 0.2, 1600)
-    print("Started WC2013")
-    elo_calculator("WC2013", 0.2, 1600)
-    print("Started WC2015")
-    elo_calculator("WC2015", 0.2, 1600)
-    print("Started WC2017")
-    elo_calculator("WC2017", 0.2, 1600)
-    print("Started WC2019")
-    elo_calculator("WC2019", 0.2, 1600)
-    print("Finished")
 
 elo_table = {}
 elo_table["personId"] = list(elo_dict.keys())
@@ -142,7 +187,7 @@ for e in range(len(events)):
     elo_table[event] = []
     for wcaid in list(elo_dict.keys()):
         score = elo_dict[wcaid][e]
-        elo_table[event].append(round(score, 2))
+        elo_table[event].append(round(score))
 
 elo_df = pd.DataFrame(elo_table)
 if not start:
