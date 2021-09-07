fe402516c3e21fbef8a0ddba857d9e677ba65ce0
Thomas Stokes <70324434+ThomasStokes1998@users.noreply.github.com>
2021-09-07 21:52:11 +0100

    Add files via upload

--- Speed Solving Analysis/Unlucky/almostWR.py
+++ Speed Solving Analysis/Unlucky/almostWR.py
@@ -33,7 +33,7 @@ three = results[results.eventId == "333"].reset_index(drop="index")
 
 def res(comp, bestmax, data=three):
     eve = data[data.competitionId == comp].reset_index(drop="index")
-    eve = eve[eve.average > 0].reset_index(drop="index")
+    # eve = eve[eve.average > 0].reset_index(drop="index")
     return eve[eve.best < bestmax].reset_index(drop="index")
 
 
@@ -77,117 +77,153 @@ def getWRs(event="333"):
 
 # Main loop
 debug = False
-event = "skewb"
-data = results[results.eventId == event].reset_index(drop="index")
-eventcomps = data.competitionId.unique()
-wravg = getWRs(event).average[0]
-main_dict = {}
 
-# Detailed list
-wcaids = []
-komp = []
-v1 = []
-v2 = []
-v3 = []
-v4 = []
-bpa = []
-cwr = []
-mint = []
+meanevents = ["333fm"]
+for event in meanevents:
+    data = results[results.eventId == event].reset_index(drop="index")
+    eventcomps = data.competitionId.unique()
+    wravg = getWRs(event).average[0]
+    main_dict = {}
 
-competitions = competitions[competitions.cancelled == 0].reset_index(drop="index")
-comps = competitions.id.unique()
-u = datetime.now()
-count = 0
-print("Started Loop")
-for comp in comps:
-    count += 1
-    if comp in eventcomps:
-        df = res(comp, wravg, data)
-        l = len(df)
-        if l > 0:
-            for i in range(l):
-                if df.average[i] < wravg:
-                    wravg = df.average[i]
-                else:
-                    a = df["value1"][i]
-                    b = df["value2"][i]
-                    c = df["value3"][i]
-                    d = df["value4"][i]
-                    # If there is a DNF in the average
-                    if np.min([a, b, c, d]) < 0 and a + b + c + d - np.min([a, b, c, d]) < 3 * wravg:
-                        wcaid = df.personId[i]
-                        # First DF
-                        if wcaid not in main_dict.keys():
-                            main_dict[wcaid] = 1
+    # Detailed list
+    wcaids = []
+    komp = []
+    v1 = []
+    v2 = []
+    v3 = []
+    v4 = []
+    bpa = []
+    cwr = []
+    mint = []
+
+    competitions = competitions[competitions.cancelled == 0].reset_index(drop="index")
+    comps = competitions.id.unique()
+    u = datetime.now()
+    count = 0
+    print(f"Started {event}")
+    for comp in comps:
+        count += 1
+        if comp in eventcomps:
+            df = res(comp, wravg, data)
+            l = len(df)
+            if l > 0:
+                # AO5 Format
+                if event not in meanevents:
+                    for i in range(l):
+                        if df.average[i] < wravg:
+                            wravg = df.average[i]
                         else:
-                            main_dict[wcaid] += 1
+                            a = df["value1"][i]
+                            b = df["value2"][i]
+                            c = df["value3"][i]
+                            d = df["value4"][i]
+                            # If there is a DNF in the average
+                            if np.min([a, b, c, d]) < 0 and a + b + c + d - np.min([a, b, c, d]) < 3 * wravg:
+                                wcaid = df.personId[i]
+                                # First DF
+                                if wcaid not in main_dict.keys():
+                                    main_dict[wcaid] = 1
+                                else:
+                                    main_dict[wcaid] += 1
 
-                        # Second DF
-                        wcaids.append(wcaid)
-                        komp.append(comp)
-                        v1.append(a / 100)
-                        v2.append(b / 100)
-                        v3.append(c / 100)
-                        v4.append(d / 100)
-                        bpa.append(round((a + b + c + d - np.min([a, b, c, d])) / 300, 2))
-                        cwr.append(wravg / 100)
+                                # Second DF
+                                wcaids.append(wcaid)
+                                komp.append(comp)
+                                v1.append(a / 100)
+                                v2.append(b / 100)
+                                v3.append(c / 100)
+                                v4.append(d / 100)
+                                bpa.append(round((a + b + c + d - np.min([a, b, c, d])) / 300, 2))
+                                cwr.append(wravg / 100)
 
-                        x = np.percentile([a, b, c, d], 200 / 3)
-                        y = np.max([a, b, c, d])
-                        t = 3 * wravg - x - y
-                        mint.append(t / 100)
+                                x = np.percentile([a, b, c, d], 200 / 3)
+                                y = np.max([a, b, c, d])
+                                t = 3 * wravg - x - y
+                                mint.append(t / 100)
 
-                    # If there are no DNFs in the average
-                    elif np.min([a, b, c, d]) > 0 and a + b + c + d - np.max([a, b, c, d]) < 3 * wravg:
-                        wcaid = df.personId[i]
-                        # First DF
-                        if wcaid not in main_dict.keys():
-                            main_dict[wcaid] = 1
+                            # If there are no DNFs in the average
+                            elif np.min([a, b, c, d]) > 0 and a + b + c + d - np.max([a, b, c, d]) < 3 * wravg:
+                                wcaid = df.personId[i]
+                                # First DF
+                                if wcaid not in main_dict.keys():
+                                    main_dict[wcaid] = 1
+                                else:
+                                    main_dict[wcaid] += 1
+
+                                # Second DF
+                                wcaids.append(wcaid)
+                                komp.append(comp)
+                                v1.append(a / 100)
+                                v2.append(b / 100)
+                                v3.append(c / 100)
+                                v4.append(d / 100)
+                                bpa.append(round((a + b + c + d - np.max([a, b, c, d])) / 300, 2))
+                                cwr.append(wravg / 100)
+
+                                x = np.percentile([a, b, c, d], 100 / 3)
+                                y = np.percentile([a, b, c, d], 200 / 3)
+                                t = 3 * wravg - x - y
+                                mint.append(t / 100)
+                # MO3 Format
+                elif event in meanevents:
+                    for i in range(l):
+                        if 0 < df.average[i] < wravg:
+                            wravg = df.average[i]
                         else:
-                            main_dict[wcaid] += 1
+                            a = df["value1"][i]
+                            a = a * 100
+                            b = df["value2"][i]
+                            b = b * 100
+                            if a > 0 and b > 0 and a + b < 3 * wravg:
+                                if event != "333fm" or event == "333fm" and 3 * wravg - a - b > 1500:
+                                    wcaid = df.personId[i]
+                                    # First DF
+                                    if wcaid not in main_dict.keys():
+                                        main_dict[wcaid] = 1
+                                    else:
+                                        main_dict[wcaid] += 1
 
-                        # Second DF
-                        wcaids.append(wcaid)
-                        komp.append(comp)
-                        v1.append(a / 100)
-                        v2.append(b / 100)
-                        v3.append(c / 100)
-                        v4.append(d / 100)
-                        bpa.append(round((a + b + c + d - np.max([a, b, c, d])) / 300, 2))
-                        cwr.append(wravg / 100)
+                                    # Second DF
+                                    wcaids.append(wcaid)
+                                    komp.append(comp)
+                                    v1.append(a / 100)
+                                    v2.append(b / 100)
+                                    cwr.append(wravg / 100)
+                                    t = 3 * wravg - a - b
+                                    mint.append(t / 100)
 
-                        x = np.percentile([a, b, c, d], 100 / 3)
-                        y = np.percentile([a, b, c, d], 200 / 3)
-                        t = 3 * wravg - x - y
-                        mint.append(t / 100)
+        if count % 1000 == 0:
+            dt = datetime.now() - u
+            print(f"Time elapsed for {count} competitions: {dt}")
+        if debug and count % 1000 == 0:
+            personId = list(main_dict.keys())
+            misses = []
+            for i in range(len(personId)):
+                misses.append(main_dict[personId[i]])
+            s = pd.DataFrame({"Name": personId, "Missed Chances": misses})
+            s.to_csv(f"unlucky {event} {count}.csv", index=False)
 
-    if count % 100 == 0:
-        dt = datetime.now() - u
-        print(f"Time elapsed for {count} competitions: {dt}")
-    if debug and count % 1000 == 0:
-        personId = list(main_dict.keys())
-        misses = []
-        for i in range(len(personId)):
-            misses.append(main_dict[personId[i]])
-        s = pd.DataFrame({"Name": personId, "Missed Chances": misses})
-        s.to_csv(f"unlucky {event} {count}.csv", index=False)
+    print("Total elapsed time :", datetime.now() - u)
+    # First dataframe
+    personId = list(main_dict.keys())
+    misses = []
+    for i in range(len(personId)):
+        misses.append(main_dict[personId[i]])
 
-print("Total elapsed time :", datetime.now() - u)
-# First dataframe
-personId = list(main_dict.keys())
-misses = []
-for i in range(len(personId)):
-    misses.append(main_dict[personId[i]])
+    stat = pd.DataFrame({"Name": wcaname(personId), "Country": wcacountry(personId), "Missed Chances": misses})
+    if __name__ == "__main__":
+        print(stat.sort_values("Missed Chances", ascending=False).reset_index(drop="index").head(10))
+        stat.to_csv(f"unlucky {event}.csv", index=False)
 
-stat = pd.DataFrame({"Name": wcaname(personId), "Country": wcacountry(personId), "Missed Chances": misses})
-if __name__ == "__main__":
-    print(stat.sort_values("Missed Chances", ascending=False).reset_index(drop="index").head(10))
-    stat.to_csv(f"unlucky {event}.csv", index=False)
+    # Second DF
+    if event not in meanevents:
+        mc = pd.DataFrame(
+            {"Name": wcaname(wcaids), "Country": wcacountry(wcaids), "Competition": komp, "Best Possible Average": bpa,
+             "Current WR": cwr, "Minimum Time Needed": mint, "Solve1": v1, "Solve2": v2, "Solve3": v3, "Solve4": v4})
+    else:
+        mc = pd.DataFrame(
+            {"Name": wcaname(wcaids), "Country": wcacountry(wcaids), "Competition": komp, "Minimum Time Needed": mint,
+             "Current WR": cwr, "Solve1": v1, "Solve2": v2})
 
-# Second DF
-mc = pd.DataFrame(
-    {"Name": wcaname(wcaids), "Country": wcacountry(wcaids), "Competition": komp, "Best Possible Average": bpa,
-     "Current WR": cwr, "Minimum Time Needed": mint, "Solve1": v1, "Solve2": v2, "Solve3": v3, "Solve4": v4})
-
-if __name__ == "__main__":
-    mc.to_csv(f"unlucky {event} breakdown.csv", index=False)
+    if __name__ == "__main__":
+        mc.to_csv(f"unlucky {event} breakdown.csv", index=False)
