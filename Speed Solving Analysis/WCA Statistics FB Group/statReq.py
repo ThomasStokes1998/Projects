b4a015e66160977b61ae15ea80fa203136f465a9
Thomas Stokes <70324434+ThomasStokes1998@users.noreply.github.com>
2021-10-29 08:37:09 +0100

    Add files via upload

---
+++ Speed Solving Analysis/WCA Statistics FB Group/statReq.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+low_memory = False
+results = pd.read_csv('WCA Database/WCA_export_Results.tsv', delimiter='\t')
+persons = pd.read_csv('WCA Database/WCA_export_Persons.tsv', delimiter='\t')
+
+persons = persons[persons.subid == 1].reset_index(drop="index")
+
+
+# A function that checks how many solves someone has done in a competition
+
+def competitionSolves(wcaid: str, compid: str):
+    solves = results[results.personId == wcaid].reset_index(drop="index")
+    compsolves = solves[solves.competitionId == compid].reset_index(drop="index")
+    lcs = len(compsolves)
+    if lcs == 0:
+        return print("Enter a valid competition.")
+    total_solves = 0
+    for i in range(lcs):
+        eventid = compsolves.eventId[i]
+        # See README.md file for how multibld scrambles are encoded
+        if eventid == "333mbf":
+            for j in range(1, 6):
+                attempt = compsolves[f"value{j}"][i]
+                if attempt > 0:
+                    dd = attempt // 10_000_000
+                    mm = attempt % 100
+                    total_solves += 99 - dd + mm
+        elif eventid == "333mbo":
+            for j in range(1, 6):
+                attempt = compsolves[f"value{j}"][i]
+                if attempt > 0:
+                    x = attempt // 10_000_000
+                    total_solves += 199 - x
+        else:
+            for j in range(1, 6):
+                if compsolves[f"value{j}"][i] > 0:
+                    total_solves += 1
+    return total_solves
+
+
+if __name__ == "__main__":
+    print(competitionSolves("2020DWOR01", "MontpellierOpen2021"))
